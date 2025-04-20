3f3f9955f1ade0b759fece678da785a172bf72a8
Ely Deckers <e.deckers@gmail.com>
2025-04-20 22:33:03 +0200

    feat: add parser

---
+++ rom.js
@@ -0,0 +1,336 @@
+frogger = "eNiiAIqVAJqG2Ibg6ND2IOL8qR2FvKn+hb2Fv4XFhcGFw4W7hbmpKIW+qZWFyqlzhcmFyKlQhbCp" +
+"DIX4qf2F6an9hdWF16kehbOpAYXehd2F0oXTIIT8IOv8qQSF0IXRheOiCr3h/ZWAvez9lYvKEPOF" +
+"AqkChQKFAYUChQKl29AS5salxskE0ASpAIXGqr39/IW+qQKFAoUAhQKFAqkAhQKFAKmajZYC5tul" +
+"3skQ0DmlPDAMpd1qsASlPTADTFT2pdvQHqXdarAGpeZJAYXmpbPQEubo8AapgMXo0AWpmiD2/EyV" +
+"9cazEPnJAdAKxq/QHcaz0BnwSckI8AfJgNDjTA3ypeHwBMbhEASl2TADTAr0pcjJjtBSpd1qpeaw" +
+"GEkBheapAIXHheWpH4WuJeOF46kEhbOl5qq00BA3pd1qkB+pEIXeqSiFs6kAheWpBIXShdMghPyp" +
+"uCD2/CDi/NBGikkBheaqtNAw1yDZ/NAEqQCV4yCE/KmAhd6pc4XIqR6F4Klbhfup/4WsqVCFsKko" +
+"hbSpDIX4pa3JB/AKpfkpP4X5qQCF+kwK9IXtSkpKSjhl7UpKSkqF7hhl7Un/CgoKCgXuGGmDYKbm" +
+"+Bh1zpXOqQB1zNi0zJXMKfCF9Jgp8MX08Ai10MkEsAL20GCpAKIDlczKEPtgCAgICAgQCCABAgID" +
+"AgEDAQAIGAg4ABgApdtqsANMHfOkrBADTBfzqQGF7sAF8Gy5gACF6rG4qr318YXrvf3xhey9BfKF" +
+"7aWwxeqwFmkHxeqwYqXqZevJoZAd6aDFsLBUkBWl6hhl68WwsEml6skIsAZpmMWwkD3G7PAOpeoY" +
+"Zeum7SCo/IXq0L3G7hADTBHzpKy5iwCF6rG6EJuxwslQ8HulkBhpD4XqpbDF6rBuaQjF6pBope7w" +
+"KMAK0DexwMm48DGliqIgIKj8herFsLADTOjyogqlsCCo/MXqkBZMEfOpBsWs8AapCcWs0Aeosb7J" +
+"C/AppKzABrAFIGH88CqpQKbm8AKpgCXf8AqlsMmTsOnJBJDlqYAF54Xn0AykrMAGsNepfyXnheep" +
+"PyXb0CDG4PAXqZg45eAgqPGF+6kGxeDQDKlvIH388AUgYfzG4KXeyYDwA0zd8yDZ/NAcleOpAIXl" +
+"qQOFsyDr/KkIhd6pmSDG8an/heH20qXl8CnGr9BopeMpQPAGpeNJwNBWpeMpH4XjqQCF5aXbha9q" +
+"ogSwAqIDhrPQQ8av0D/Gs9A7pdspA6jIhOqpADgqxurQ+/ABKqbmNePwDMjABhjQ86ABmBiQ7oTl" +
+"pua10mqiILACokCKBeOF46mdha+pgCXn8CCkrDAcscQl29AWprC5R/0wB6kBIKj80AbKitACqaCF" +
+"sKWwIKjxhbSkrcAH0A+xxCXb0ALmsaWxIKjxhbWkrsAI0BelxylA0BGxxCXb0AvmsuaypbIgqPGF" +
+"tqX5MAcpQPAnTNz0pdspP9BKpfrJAqWxkAXpCExc9GkIhbGm+ujgBNACogCG+hBfpYfJBNBroAex" +
+"xCXb0GOl+UkghfkpIPBZqQCFsYX6qXOFtakHha2pwIX50EWlrMkH0C2lyMmO8CelsMWxkAjpBsWx" +
+"kAiwGWkGxbGQE6kIhfipf4WtJfmF+aloIH388BKlscmYkAyp/4WtpfkJICk/hfmm5qkE1dKQA0yV" +
+"9akIxa7QfMWs0DOlyMmO8C2pQCXHheylssWwsBOm7NAdGGkPxbCQFukIxbCwEJALpuzwCjjpCMWw" +
+"sAMgYfylssmYsDSpQCXH0Bmlk2kUyUCQXcWysFmpQAXHhcepnIXKTJX1pYjpBMWykESpvyXHhcep" +
+"lYXKTJX1qYCFriXHhcdMlfWliMkB0CagCLHEJdvQHqXHSYCFxxAWqQCFsqlzhbapCIWuqZWFyqm/" +
+"JceFx6bmtdKF6skB8EOl2ylA8ASpqNACqbOFwKkDxeqwL6XbKRDQBKnJ0AKpvoXCpdspENAPopWl" +
+"xylA8AKinIbKTOL1olylxylA8AKiYobKpNkwN9AmsdbwK0iiASkPqLmP/ZUXMASpBdACqQyVFcow" +
+"Bmgg1PwQ5+bWoA+IhBmEGoTZECzG2cbYMCak2DAi0B6x1IUZ8Bgg1PyqvVj9hRXIsdSFFyDU/EqF" +
+"2ObU5tTG2KXbarADTCn3rYIChd8pAdBcpt2pA+AD8AbgBPACqQGF0oXTogCG5objhuSG+Yb6huXK" +
+"hqypmiD2/CCE/KkEhdCF0YWzqVCFsKkohbSpDIX4qR6F4KlbhfuFrYWuqXOFyKkuhegg6/GpCIXe" +
+"0Gel3ykC8Amp3yXnhedMF/eF5iDr8SDi/KkehbOpICXn0EapIAXnheepAYXept3o4AfQAqIBht2l" +
+"3aIEarACogqG46kAheWpe+AE0AKpYSB9/IXmpd2iA8kD8AbJBPACogGG0obTIIT8pd7JEPAMpd+g" +
+"LikI0AKgH4ToqQCFDaAAseiFCa2EAjD7qUCFAoUBqYyNlgKl22qQV6Xl8FOl46IAKrAE6OjQ+b0U" +
+"/oXzvRX+hfSpAIXvhfCpUIX1qf+F9qXlyQSQAukDqqkAOCrK0PwqqqTlvQz+wASwA738/YXxpuWp" +
+"o+AEsAKpOoXyqQzQSqbmteMpH4XqKQfQBqJQoP/QBKLVoP2G84T0GCqqvfz9hfG9/f2F76Xqamop" +
+"BtAGolCg/9AEotWg/Yb1hPaqvQz+hfK9Df6F8KkEhfem5qXeyQHQAqICvVX9heu9+f+F7r1S/YXq" +
+"ogAgtPyl6+ggw/ygArHohQaFB6IBhgSGBYYKogeahQKl3skB8DOm5qAAtcwpDwoKCpnrALXMKfBK" +
+"meoA6OjIyMAE0OaFAqIAterwAtAcqVCV6ujgA9Dx8BGFAqBQhOuE7ITtpd0KCgqF6q2EAjD7hQKk" +
+"7ogQ/aTquQH/hRuk67kB/4UcpOy5Af+qpO25Af+GG4Uc5urm6+bs5u3m7rrKmhDUqQWFCqIBpfCF" +
+"BaXyILT8oASx6IUJhQik97HohQaFB8ql74UEpfEgw/ygC7HohQmpMIUNqTyFDoUPoAux84UChRux" +
+"9YUciNDzhAKEG4QchA6ED6AKqf+F7IXuhOrABfAKhQKl22qQA0zK+bG6hQWxwoXtuaEAogEgtPyk" +
+"6sAF0CGgA7HohQmgBcSs0BWk+LHohQalyIXrpbSiAIYEIMP8MBixuIUEscCF67mWAMogw/yk6rG8" +
+"qLHohQak6rG+qLHohQegB4UCseuFG7HthRyI0POFAoQbhByk6ogwEcAE0AqgAIUCseiFCaAETAD5" +
+"oAOx6IX3ogCltCDD/KX3hQmk+LHohQapAIUEpcimrDACqVCF66AHhQKx64UbiND3hQKEG0w9+sSt" +
+"8AvErvA9xKzwRUwQ+aIAhgWgAabJpbWG7YTvogEgtPypUKSsxOrQAqXIhevKhgSltCDD/KTvseiF" +
+"B6T4seiFBkxo+akFhQWmyqW2oA7QyKT4seiFBqkAhQSlyIXrpbSiACC0/KlQhe2ltOggw/xMaPmi" +
+"B6XgyRCwAqIFhgSgAKkGxeCQAqAJseiFBqkFhQWpRKIBILT8oASx6IUJoAGx6IUHoP+l4BAByITv" +
+"pfvKIMP8pua10BAEoADwBykHqr33/aiiBYUChByl74UbytD1hhuGHKCnjJYCpdtqkAal3smA8ANM" +
+"+futgAKm5vAECgoKCkn/KfDFy4XL0Ayl2ykG0OKm3eAFkNyly8kQ8DLJIPAkyUDwEsmA0MqlsGkH" +
+"yZOwwoWwqYCQLaWw6QjJBpC0hbCph9AfpawwqsasqXnQFeaspqzgC/AX5NqQB+baqQEgxvGpc4XI" +
+"qWEgffxM+fvGrKkLheqiBaSwxOqQFqkHZeqF6sTqkBLK8AmpGBhl6oXq0OYgYfxM+fuG66kGOOXr" +
+"hevF5dAbpeOF7CkfheOpAIXlqQKFsyTsMNlwBakgIMbxpua14ykfheo4qQCm6yrK0Pwk6vADTPn7" +
+"puYV45XjpfkwCClA8ASpJdACqQUgxvGgAITaqUmFrMil2ykDqr3h/ZWAvez9lYuIMAPo0PAg2fzw" +
+"CKkIheGpCIXeOKL/peDo6Qqw+2kKheqKCgoKCgXqher4ZerYIMbxqYEgffygCqkBhe6xxCXb0FG2" +
+"gNACtou5R/0wCejgodAJogHQBcrQAqKgpe7QBJaL8AKWgIqF6kpKSko4ZepKSkpKhesYZepJ/woK" +
+"CgoF6xhpg6bu0AWZoQDwA5mWAMbuELOIEKKthAIw+0xx8KkIhd6pRoXhqY6FyKl/JeeF56bm1tCp" +
+"AIXaqYiF1KkAhdhgpua10skLkATpCtD4qL48/Ya4hrq5AP2FxLkK/YXAuRT9hcJghvMYZfPJoZAC" +
+"6aBglSApD6iFAogQ/ZUQhQJglSApD6iFAogQ/ZUQhQKFKmBKSkpKYKbmteMpH8kfYKJAhqyGrYau" +
+"YKbmtdJqqZqQAqm4hdapAIXZYCgzPklUXmhzkodzXod9nbOoqLOzs7Ozs52dnb7JvrOzs7MADgwI" +
+"AgMADQ4ACgQODA4ADigc1JASaBYilpDWWNTq9dT16vXq39TfnwCfAJ8AnwAAnwDFycc2OjgECA3t" +
+"Dg6hAh8JbQhnBnAADKgMpAyoAC5CKEMiRS5iKEMiRS5CKEMiRS5CKEMiRQAecCZFGJ4Al5SRjxoX" +
+"FBMRDwBiQkBCYkJAQXNzYWNTo6Cjc3NgY1NTkJODc2BTQgBCUGJwgqBioEJQYlBCoEKgQlBicIKg" +
+"YqCDcGNQQgDDwzxm2/+9POelQgELDhYPCAVaCkYCUVteZl8yRmIqClIAgKCoqqMAowB2AKMCWACj" +
+"BHYCowY6ADoAHAA6Amj/4v/t/wAAmgIHDg0BDgkGBgkGAgcODQEOCQYGCgYCBw4NAQ4KBgYLBgIH" +
+"Dg0BDgsGBgoGAgcODQEOCgYGCQYDBwcDBwEDAQMBAQMDAQMAAQMBAwMBAwEDAAEBAAMBAAEDAQAD" +
+"BwMBAQMAAQMAAwMHAwEBAAAHAAcBAwEDBwADAAMBAQABAwEBxtTNv9tQsbi4sbjG1M2/21CxuLix" +
+"o8bUzb/bULG4uLGqxtTNv9uVsbi4sbjG1M2/21yxuLixuAEGAAMFBQMCBwEHAwYDBgUFAwQHAQcA" +
+"AAIABQUDBgcBBwIBAQEFBQMGBwEHABwiY2NjIhwABAwcDAwMfwA+AwM+YGB/AH4DAz4DA34AAgYO" +
+"FiZ/BgB+YGA+AwN+AD5gYH5jYz4Af2EDBgwYMAA+Y2M+Y2M+AD5jYz8DAz4AAAAAAAAAAAAAAAAA" +
+"AAgdvmcCABC4feZAAPj/1YDA4PC4XAaCkv44fLqSunw4/pKC7jh8fnw47nccPn4+HHeBQn48JEKB" +
+"iFh8JQYDAhEaPqRgwEArLvz8fjsVKC/8/344FDx+////fjz933/7/+//xgD/x/8AxmMA/+P/AGNm" +
+"/sfmx/5m/Xn/mf95/V///////18A4KAAAAAAAIDAYAQOHFz69/pcHA4EDhIQAPAA8A=="
+
+const printHex = false;
+const printAsm = true;
+
+const operators = {
+  "ADC #nn": [0x69, 1],
+  "ADC nn": [0x65, 1],
+  "ADC nn, X": [0x75, 1],
+  "AND #nn": [0x29, 1],
+  "AND nn": [0x25, 1],
+  "ASL A": [0x0a, 0],
+  "BCC dd": [0x90, 1],
+  "BCS dd": [0xb0, 1],
+  "BEQ dd": [0xf0, 1],
+  "BMI dd": [0x30, 1],
+  "BNE dd": [0xd0, 1],
+  "BPL dd": [0x10, 1],
+  "BRK": [0x00, 0],
+  "CLC": [0x18, 0],
+  "CLD": [0xd8, 0],
+  "CMP nn": [0xc5, 1],
+  "CMP #nn": [0xc9, 1],
+  "CPX #nn": [0xe0, 1],
+  "CPY #nn": [0xc0, 1],
+  "CPY nn": [0xc4, 1],
+  "DEC nn": [0xc6, 1],
+  "DEX": [0xca, 0],
+  "DEY": [0x88, 0],
+  "EOR nn": [0x49, 1],
+  "INC nn": [0xe6, 1],
+  "INC nn, X": [0xf6, 1],
+  "INX": [0xe8, 0],
+  "INY": [0xc8, 0],
+  "JMP nnnn": [0x4c, 2],
+  "JSR nnnn": [0x20, 2],
+  "LDA #nn": [0xa9, 1],
+  "LDA (nn), Y": [0xb1, 1],
+  "LDA nn": [0xa5, 1],
+  "LDA nn, X": [0xb5, 1],
+  "LDA nnnn": [0xad, 2],
+  "LDA nnnn, X": [0xbd, 2],
+  "LDA nnnn, Y": [0xb9, 2],
+  "LDY #nn": [0xa0, 1],
+  "LDY nn, X": [0xb4, 1],
+  "LDY nn": [0xa4, 1],
+  "LDX #nn": [0xa2, 1],
+  "LDX nn": [0xa6, 1],
+  "LSR A": [0x4a, 0],
+  "ORA nn": [0x05, 1],
+  "ORA (nn, X)": [0x01, 1],
+  "PHP": [0x08, 0],
+  "ROL A": [0x2a, 0],
+  "ROR A": [0x6a, 0],
+  "RTS": [0x60, 0],
+  "SBC #nn": [0xe9, 1],
+  "SBC (nn, X)": [0xe1, 1],
+  "SEC": [0x38, 0],
+  "SED": [0xf8, 0],
+  "SEI": [0x78, 0],
+  "STA nn": [0x85, 1],
+  "STA nn, X": [0x95, 1],
+  "STA nnnn": [0x8d, 2],
+  "STA nnnn, Y": [0x99, 2],
+  "STX nn": [0x86, 1],
+  "STY nn": [0x84, 1],
+  "TAX": [0xaa, 0],
+  "TAY": [0xa8, 0],
+  "TSX": [0xba, 0],
+  "TXA": [0x8a, 0],
+  "TXS": [0x9a, 0],
+  "TYA": [0x98, 0],
+};
+
+const operatorLookup = Object.fromEntries(Object.entries(operators).map(([k, v]) => [v[0], [k, v[1]]]));
+
+const branches = new Set([
+  0x90,
+  0xb0,
+  0xf0,
+  0x30,
+  0xd0,
+  0x10,
+  0x20, // JSR
+]);
+
+const jumps = new Set([
+  0x00, // BRK
+  0x4c, // JMP
+]);
+
+const stops = new Set([
+ 0x60, // RTS
+]);
+
+const code = (v)  => v.toString(16).padStart(2, "0");
+
+const offs = (a) => a - 0xf000;
+
+const read = (input, a, bc) => {
+  const zs = [];
+
+  for (var b=0; b<bc; b++) {
+    zs.push(input[offs(a + b)]);
+  }
+
+  zs.reverse();
+
+  return zs.reduce((p, c) => (p << 8) + c, 0);
+}
+
+function scan(input) {
+  const entrypoint = read(input, 0xfffc, 2)
+
+  const reachable = new Set([]);
+
+  const follow = (ix, pc) => {
+    while (pc <= 0xffff) {
+      if (reachable.has(pc)) {
+        return;
+      }
+
+      reachable.add(pc);
+      
+
+      const operator = read(ix, pc, 1);
+      // console.log(pc.toString(16));
+      // console.log(pc.toString(16), operator.toString(16).padStart(2, "0"));
+      const [_, l] = operatorLookup[operator];
+      const next = pc + l + 1
+
+      // console.log(pc.toString(16), formatASM(toASM(input, pc)));
+
+      if (jumps.has(operator)) {
+	// console.log("JMP");
+	const target = read(ix, pc + 1, l);
+        follow(ix, target);
+        break;
+      } else if (branches.has(operator)) {
+	// console.log("COND");
+
+	const relative = next + read(ix, pc + 1, l)
+        // const direction = (relative & 0x80) === 0x80 ? -1 : 1;
+	const direction = 1;
+
+	const target = pc + (relative * direction);
+
+	// console.log("T", target.toString(16).padStart(2, "0"));
+
+        follow(ix, target);
+      } else if (stops.has(operator)) {
+	return;
+      }
+
+      pc = next;
+    }
+  }
+
+  follow(input, entrypoint);
+
+  return reachable;
+}
+
+
+function formatHex(input, columns = 10) {
+  const operations = [];
+
+  for (i = 0; i < input.length; i += columns) {
+    operations.push(input.slice(i, i + columns).map(v => v.padStart(2, "0")).join(" "));
+  }
+
+  return operations.join("\n");
+}
+
+function formatASM(line) {
+  const [ops, name] = line;
+
+  const codeAsHex = ops.map(c => c.toString(16).padStart(2, "0"));
+
+  const operand = ops.slice(1);
+
+  return [
+    codeAsHex.join(" ").padEnd(8, " "),
+    name
+	  .replace("nnnn", "nn")
+	  .replace("dd", "nn")
+	  .replace("nn", operand.reverse().map(o => o.toString(16).padStart(2, "0")).join(""))
+  ].join(" ");
+}
+
+function toASM(input, addr) {
+  const pc = offs(addr)
+  const operator = input[pc]
+
+  if (!(operator in operatorLookup)) {
+    return [[operator], "Unkown"]
+  }
+
+  const [name, operandCount] = operatorLookup[parseInt(operator, 10)];
+
+  const operandBytes = [];
+  for (i = 0; i < operandCount; i++) {
+    operandBytes.push(input[pc + i + 1]);
+  }
+
+  return [[operator].concat(operandBytes), name];
+}
+
+
+const decode = (input) => {
+  const reachable = scan(input);
+
+  const lines = [];
+  var data = [];
+
+  const printData = (pc) => {
+      return [
+	".data",
+	[(pc - data.length).toString(16), "...", pc.toString(16)].join(""),
+	data.map(c => c.toString(16).padStart(2, "0")).join(" ")
+      ].join(" ")
+  }
+
+  let pc = 0xf000;
+  while (pc <= 0xffff) {
+    if (!(reachable.has(pc))) {
+       data.push(read(input, pc, 1));
+       pc++;
+       continue;
+    }
+
+    if (data.length > 0) {
+      lines.push(printData(pc));
+      data = [];
+    }
+
+    const operator = read(input, pc, 1);
+    const [_, l] = operatorLookup[operator];
+
+    lines.push([
+      pc.toString(16),
+      formatASM(toASM(input, pc))].join(" "));
+
+    pc += l + 1;
+  }
+
+  lines.push(printData(0xffff));
+
+  return lines;
+}
+
+function loadFromBase64(input) {
+  const romInBytes = atob(input).split("").map(c => c.charCodeAt(0));
+
+
+  if (printHex) {
+    console.log(formatHex(romInBytes.map(b => b.toString(16))));
+  }
+
+  const lines = decode(romInBytes);
+
+  if (printAsm) {
+    console.log (lines.join("\n"));
+  }
+}
