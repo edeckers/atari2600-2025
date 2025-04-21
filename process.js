f695d3156f10f1e55c10f20f33df572459521bfb
Ely Deckers <e.deckers@gmail.com>
2025-04-21 02:49:47 +0200

    refactor: read, write through same interface

--- process.js
+++ process.js
@@ -35,40 +35,53 @@ const tc = (v) => {
 }
 
 const processors = {
-  /* JSR nnnn    */ 0x20: (rom) => { sram(sp, (pc >> 8) & 0xff); sram(sp + 1, pc & 0xff); sp += 2; pc = (rrom(rom, pc + 2) << 8) + rrom(rom, pc + 1); },
-  /* RTS         */ 0x60: () => { sp -= 2; h = rram(sp, 8) << 8; l = sram(sp); pc = h + l; },
+  /* JSR nnnn    */ 0x20: (read, write) => { write(sp, (pc >> 8) & 0xff); write(sp + 1, pc & 0xff); sp += 2; pc = (read(pc + 2) << 8) + read(pc + 1); },
+  /* RTS         */ 0x60: (read, write) => { sp -= 2; h = read(sp, 8) << 8; l = write(sp); pc = h + l; },
   /* SEI         */ 0x78: () => { fi = 1; },
-  /* STA nn      */ 0x85: (rom) => { sram(rrom(rom, pc + 1), ra & 0xff); },
-  /* STX nn      */ 0x86: (rom) => { sram(rrom(rom, pc + 1), rx & 0xff); },
+  /* STA nn      */ 0x85: (read, write) => { write(read(pc + 1), ra & 0xff); },
+  /* STX nn      */ 0x86: (read, write) => { write(read(pc + 1), rx & 0xff); },
   /* TXA         */ 0x8a: () => { ra = rx; },
-  /* BCC dd      */ 0x90: (rom) => { fc === 0 && (pc += tc(rrom(rom, pc + 1)) + 2); },
-  /* STA nn, X   */ 0x95: (rom) => { sram(rrom(rom, pc + 1) + rx, ra & 0xff); },
+  /* BCC dd      */ 0x90: (read) => { fc === 0 && (pc += tc(read(pc + 1)) + 2); },
+  /* STA nn, X   */ 0x95: (read, write) => { write(read(pc + 1) + rx, ra & 0xff); },
   /* TXS         */ 0x9a: () => { sp = rx; },
-  /* LDX #nn     */ 0xa2: (rom) => { rx = rrom(rom, pc + 1); },
-  /* LDA #nn     */ 0xa9: (rom) => { ra = rrom(rom, pc + 1); },
-  /* LDY nnnn    */ 0xac: (rom) => { addr = rrom(rom, pc + 2) << 8 + rrom(rom, pc + 1); ry = rram(addr); }, // This 2 byte addressing won't work
-  /* DEC nn      */ 0xc6: (rom) => { addr = rrom(rom, pc + 1); v = (rram(addr) - 1) & 0xff; sram(addr, v); fn = tc(v) < 0; fz = tc(v) === 0;},
-  /* BNE dd      */ 0xd0: (rom) => { fz === 0 && (pc += tc(rrom(rom, pc + 1)) + 2); },
+  /* LDX #nn     */ 0xa2: (read) => { rx = read(pc + 1); },
+  /* LDA #nn     */ 0xa9: (read) => { ra = read(pc + 1); },
+  /* LDY nnnn    */ 0xac: (read) => { addr = read(pc + 2) << 8 + read(pc + 1); ry = read(addr); }, // This 2 byte addressing won't work
+  /* DEC nn      */ 0xc6: (read, write) => { addr = read(pc + 1); v = (read(addr) - 1) & 0xff; write(addr, v); fn = tc(v) < 0; fz = v === 0; },
+  /* BNE dd      */ 0xd0: (read) => { fz === 0 && (pc += tc(read(pc + 1)) + 2); },
   /* CLD         */ 0xd8: () => { fd = 0; },
-  /* SBC (nn, X) */ 0xe1: (rom) => { addr = rrom(rom, pc + 1) + rx; v = ra + fc - 1 + rram(addr) + rram(addr + 1); ra = v & 0xff; },
-  /* INX         */ 0xe8: () => { rx = (rx + 1) && 0xff; fn = tc(rx) < 0; fz = tc(rx) === 0; },
+  /* SBC (nn, X) */ 0xe1: (read) => { addr = read(pc + 1) + rx; v = ra + fc - 1 + read(addr) + read(addr + 1); ra = v & 0xff; },
+  /* INX         */ 0xe8: () => { rx = (rx + 1) && 0xff; fn = tc(rx) < 0; fz = rx === 0; },
 }
 
 const process = (rom) => {
-  const entrypoint = read(rom, 0xfffc, 2)
+  const entrypoint = romread(rom, 0xfffc, 2)
+
+  const read = (addr) => {
+    if ((addr & 0xf000) === 0xf000) { return rrom(rom, addr); };
+
+    if ((addr >= 0x0080)  && (addr <= 0x00FF)) { return rram(addr); }
+    if ((addr >= 0x0180)  && (addr <= 0x01FF)) { return rram(addr); }
+    if ((addr >= 0x0480)  && (addr <= 0x04FF)) { return rram(addr); }
+    if ((addr >= 0x0580)  && (addr <= 0x05FF)) { return rram(addr); }
+  }
+
+  const write = (addr, v) => {
+     sram(addr, v)
+  }
 
   pc = entrypoint;
 
   let i = 0;
   while (i < 200) {
-    const o = rrom(rom, pc)
-
+    const o = read(pc)
+  
     console.log(formatASM(toASM(rom, pc)));
 
     const [_, l] = operatorLookup[o]
     const p = processors[o];
 
-    p(rom);
+    p(read, write);
     pc += l + 1;
 
     printState();
